42cc51f0d955ef4e4f3cc90bdc6d113f22d7076c
Simon Skinner <vultuk28@gmail.com>
2022-01-03 09:38:21 +0000

    Changed required settings

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import { NextFunction, Request, Response } from 'express';
 
 import web3 from 'web3';
 
-import {Settings, Web3Interface} from './Types';
+import { Settings, Web3Interface } from './Types';
 
 export * from './Types';
 
@@ -14,11 +14,11 @@ declare global {
   }
 }
 
-export default (additionalSettings: Settings) => (req: Request, res: Response, next: NextFunction) => {
+export default (additionalSettings?: Settings) => (req: Request, res: Response, next: NextFunction) => {
   const settings: Required<Settings> = {
     blockchainUrl: `https://localhost:8545/`,
-    ...additionalSettings
-  }
+    ...(additionalSettings || {}),
+  };
 
   req.web3 = new web3(settings.blockchainUrl);
 
